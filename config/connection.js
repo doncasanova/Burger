5232c59f920261abb6e46515f80233d3ffbaaa96
doncasanova <ethanjuan@yahoo.com>
2018-06-21 14:25:35 -0500

    added connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,27 @@ console.log('in the connection')
 var mysql = require("mysql");
 var connection;
 
-var connection = mysql.createConnection({
-  port: 3206,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// For Heroku Deployment vs. Local MySQL Database
 
+ if(process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+ connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
+
+// var connection = mysql.createConnection({
+//   port: 3206,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 // Make connection.
 connection.connect(function(err) {
   if (err) {

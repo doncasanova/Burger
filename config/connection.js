01df9a6bf5878199d4d00cd1eb9e979214cd681c
doncasanova <ethanjuan@yahoo.com>
2018-06-21 15:07:24 -0500

    working on heroku connection

--- config/connection.js
+++ config/connection.js
@@ -5,27 +5,28 @@ var connection;
 
 // For Heroku Deployment vs. Local MySQL Database
 
- if(process.env.JAWSDB_URL){
- connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-else{
- connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root', // Add your password
-    database : 'burgers_db' // Add your database
+else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '', // Add your password
+    database: 'burgers_db' // Add your database
   });
 }
 
 // var connection = mysql.createConnection({
-//   port: 3206,
+//   port: 3306,
 //   host: "localhost",
 //   user: "root",
-//   password: "root",
+//   password: "",
 //   database: "burgers_db"
 // });
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
